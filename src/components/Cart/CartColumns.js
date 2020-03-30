91d1d2c48bcdf710b39e7e02d46d1ff56e8deb37
Shailesh Khanal <shylaysh@gmail.com>
2020-03-30 20:51:51 +0545

    Some changes made for small screens

--- src/components/Cart/CartColumns.js
+++ src/components/Cart/CartColumns.js
@@ -4,22 +4,22 @@ export default function CartColumns() {
   return (
     <div className="container-fluid text-center d-lg-block mt-5 mb-3">
       <div className="row">
-        <div className="col-10 xm-auto col-lg-2">
+        <div className="col d-none d-sm-block">
           <h4 className="text-uppercase">Products</h4>
         </div>
-        <div className="col-10 xm-auto col-lg-2">
+        <div className="col d-none d-sm-block">
           <h4 className="text-uppercase">Name of Product</h4>
         </div>
-        <div className="col-10 xm-auto col-lg-2">
+        <div className="col d-none d-sm-block">
           <h4 className="text-uppercase">Price</h4>
         </div>
-        <div className="col-10 xm-auto col-lg-2">
+        <div className="col d-none d-sm-block">
           <h4 className="text-uppercase">Quantity</h4>
         </div>
-        <div className="col-10 xm-auto col-lg-2">
+        <div className="col d-none d-sm-block">
           <h4 className="text-uppercase">Remove</h4>
         </div>
-        <div className="col-10 xm-auto col-lg-2">
+        <div className="col d-none d-sm-block">
           <h4 className="text-uppercase">Total</h4>
         </div>
       </div>
